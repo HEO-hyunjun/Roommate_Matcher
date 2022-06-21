bd91b363a6fd523cb61b70fd08be618ca9b9aa64
root <root@ip-172-31-43-183.ap-northeast-2.compute.internal>
2022-06-21 17:55:20 +0000

    a

--- includes/DbOperation.php
+++ includes/DbOperation.php
@@ -2,54 +2,43 @@
  
 class DbOperation
 {
-    //DB 연결 링크
+    //Database connection link
     private $con;
  
-    //Constructor 클래스
+    //Class constructor
     function __construct()
     {
-        //DbConnect.php 파일 참조
+        //Getting the DbConnect.php file
         require_once dirname(__FILE__) . '/DbConnect.php';
  
-        //DB에 연결하는 DbConnect 객체 생성
+        //Creating a DbConnect object to connect to the database
         $db = new DbConnect();
  
-        //connect 메소드를 호출해 연결 링크 초기화
+        //Initializing our connection link of this class
+        //by calling the method connect of DbConnect class
         $this->con = $db->connect();
     }
-     
-    
-	
-    //profiles 테이블에 새로운 레코드를 생성하는 메소드
-    function createProfile($KakaoID, $Name, $Grade, $Introduce, 
-			   $Personality, $WakeupTime, $SleepTime, $Snoring, 
-			   $Noise, $Hygiene, $Smoking, $Language){
-	    
-    	$stmt = $this->con->prepare("INSERT INTO profiles (KakaoID, Name, Grade, Introduce, 
-							   Personality, WakeupTime, SleepTime, Snoring, 
-							   Noise, Hygiene, Smoking, Language)
-				    			   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-	    
- 	$stmt->bind_param("ssisiiiiiiii", $KakaoID, $Name, $Grade, $Introduce, 
-			  		  $Personality, $WakeupTime, $SleepTime, $Snoring, 
-			  		  $Noise, $Hygiene, $Smoking, $Language);
- 	if($stmt->execute())
- 		return true; 
- 	return false; 
-    }
  
-    
-     
-     
-     //profiles 테이블에 존재하는 모든 레코드 출력하는 메소드
-     function getProfiles(){
-     	$stmt = $this->con->prepare("SELECT KakaoID, Name, Grade, Introduce, 
-					    Personality, WakeupTime, SleepTime, Snoring, 
-					    Noise, Hygiene, Smoking, Language FROM profiles");
+ /*
+ * When this method is called a new record is created in the database
+ */
+ function createProfile($KakaoID, $Name, $Grade, $Introduce, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking, $Language){
+ 	$stmt = $this->con->prepare("INSERT INTO profiles (KakaoID, Name, Grade, Introduce, Personality, WakeupTime, SleepTime, Snoring, Noise, Hygiene, Smoking, Language)
+	 			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+ 	$stmt->bind_param("ssisiiiiiiii", $KakaoID, $Name, $Grade, $Introduce, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking, $Language);
+ 	if($stmt->execute())
+ 	return true; 
+ 	return false; 
+ }
+ 
+ /*
+ * The read operation
+ * When this method is called it is returning all the existing record of the database
+ */
+ function getProfiles(){
+ 	$stmt = $this->con->prepare("SELECT KakaoID, Name, Grade, Introduce, Personality, WakeupTime, SleepTime, Snoring, Noise, Hygiene, Smoking, Language FROM profiles");
  	$stmt->execute();
- 	$stmt->bind_result($KakaoID, $Name, $Grade, $Introduce, 
-			   $Personality, $WakeupTime, $SleepTime, $Snoring, 
-			   $Noise, $Hygiene, $Smoking, $Language);
+ 	$stmt->bind_result($KakaoID, $Name, $Grade, $Introduce, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking, $Language);
  
  	$profiles = array(); 
  
@@ -69,36 +58,28 @@ class DbOperation
  		$profile['Language'] = $Language; 
 
  		array_push($profiles, $profile); 
-     }
+ 	}
  
-     return $profiles; 
+	return $profiles; 
  }
  
- 
- 
- 
- //메소드 호출 시 주어진 KakaoID에 해당하는 레코드를 새로운 입력값으로 변경
- function updateProfile($KakaoID, $Name, $Grade, $Introduce, 
-			$Personality, $WakeupTime, $SleepTime, $Snoring, 
-			$Noise, $Hygiene, $Smoking, $Language){
-	 
- 	$stmt = $this->con->prepare("UPDATE profiles SET Name = ?, Grade = ?, Introduce = ?, Personality = ?, 
-							 WakeupTime = ?, SleepTime = ?, Snoring = ?, Noise = ?, 
-							 Hygiene = ?, Smoking = ?, Language = ? WHERE KakaoID = ?");
-	 
- 	$stmt->bind_param("sisiiiiiiiis", $Name, $Grade, $Introduce, $Personality, 
-			  		  $WakeupTime, $SleepTime, $Snoring, $Noise, 
-			  		  $Hygiene, $Smoking, $Language, $KakaoID);
+ /*
+ * The update operation
+ * When this method is called the record with the given id is updated with the new given values
+ */
+ function updateProfile($KakaoID, $Name, $Grade, $Introduce, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking, $Language){
+ 	$stmt = $this->con->prepare("UPDATE profiles SET Name = ?, Grade = ?, Introduce = ?, Personality = ?, WakeupTime = ?, SleepTime = ?, Snoring = ?, Noise = ?, Hygiene = ?, Smoking = ?, Language = ? WHERE KakaoID = ?");
+ 	$stmt->bind_param("sisiiiiiiiis", $Name, $Grade, $Introduce, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking, $Language, $KakaoID);
  	if($stmt->execute())
  		return true; 
  	return false; 
  }
  
  
- 
- 
-  
- //주어진 KakaoID에 해당하는 레코드를 삭제하는 메소드
+ /*
+ * The delete operation
+ * When this method is called record is deleted for the given id 
+ */
  function deleteProfile($KakaoID){
  	$stmt = $this->con->prepare("DELETE FROM profiles WHERE KakaoID = ? ");
  	$stmt->bind_param("s", $KakaoID);
@@ -106,5 +87,168 @@ class DbOperation
  		return true; 
  
  	return false; 
- }	
+ 
+ }
+ //profiles 데이터베이스 전체 읽기
+  function geteveryProfiles(){
+    $stmt = $this->con->prepare("SELECT KakaoID, Name, Grade, Gender, Dormitory, Introduce, Personality, WakeupTime, SleepTime, Snoring, Noise, Hygiene, Smoking FROM profiles ");
+ 	$stmt->execute();
+ 	$stmt->bind_result($KakaoID, $Name, $Gender, $Dormitory, $Grade, $Introduce, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking);
+ 
+ 	$profiles = array(); 
+ 
+ 	while($stmt->fetch()){
+ 		$profile  = array();
+		$profile['Name'] = $KakaoID; 
+ 		$profile['Name'] = $Name; 
+ 		$profile['Grade'] = $Grade; 
+		$profile['Name'] = $Gender; 
+		$profile['Name'] = $Dormitory; 
+ 		$profile['Introduce'] = $Introduce; 
+ 		$profile['Personality'] = $Personality;
+ 		$profile['WakeupTime'] = $WakeupTime;
+ 		$profile['SleepTime'] = $SleepTime; 
+ 		$profile['Snoring'] = $Snoring;
+ 		$profile['Noise'] = $Noise;
+ 		$profile['Hygiene'] = $Hygiene;
+ 		$profile['Smoking'] = $Smoking;
+
+ 		array_push($profiles, $profile); 
+     }
+ 
+     return $profiles; 
+ }
+
+
+
+  //한명의 프로필을 볼 때 작동
+  function getProfiles($KakaoID){
+    $stmt = $this->con->prepare("SELECT Name, Grade, Introduce, Personality, WakeupTime, SleepTime, Snoring, Noise, Hygiene, Smoking FROM profiles WHERE KakaoID = ?");
+	$stmt->bind_param("i", $KakaoID);
+ 	$stmt->execute();
+ 	$stmt->bind_result($Name, $Grade, $Introduce, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking);
+ 
+ 	$profiles = array(); 
+ 
+ 	while($stmt->fetch()){
+ 		$profile  = array();
+ 		$profile['Name'] = $Name; 
+ 		$profile['Grade'] = $Grade; 
+ 		$profile['Introduce'] = $Introduce; 
+ 		$profile['Personality'] = $Personality;
+ 		$profile['WakeupTime'] = $WakeupTime;
+ 		$profile['SleepTime'] = $SleepTime; 
+ 		$profile['Snoring'] = $Snoring;
+ 		$profile['Noise'] = $Noise;
+ 		$profile['Hygiene'] = $Hygiene;
+ 		$profile['Smoking'] = $Smoking;
+
+ 		array_push($profiles, $profile); 
+     }
+ 
+     return $profiles; 
+ }
+
+ // UI#8 자신의 프로필에서 학년, 기숙사 자기소개등 변경
+function writeProfile($KakaoID, $Name, $Grade, $Gender, $Dormitory, $Introduce, $Profileimage){
+	$stmt = $this->con->prepare("UPDATE profiles SET  Name = ?, Grade = ?, Gender = ?, Dormitory = ?, Introduce = ?, Profileimage = ?WHERE KakaoID = ?");
+	$stmt->bind_param("siIssiii", $Name, $Grade, $Gender, $Dormitory, $Introduce, $Profileimage, $KakaoID);
+	if($stmt->execute())
+	return true;
+	return false;
 }
+
+//UI#11 나의 프로필 작성
+function pickProfile($KakaoID, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking){
+	$stmt = $this->con->prepare("UPDATE profiles SET Personality = ?, WakeupTime = ?, SleepTime = ?, Snoring = ?,Noise = ?, Hygiene = ?, Smoking = ?, WHERE KakaoID = ?");
+	$stmt->bind_param("iiiiiiiii", $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking, $KakaoID);
+	if($stmt->execute())
+	return true;
+	return false;
+
+
+}
+//정렬기능
+function sort($KakaoID, $Gender, $Dormitory){
+    $stmt = $this->con->prepare("SELECT bot.Name, bot.Grade, bot.Introduce, bot.Profileimage, bot.Personality, bot.WakeupTime, bot.SleepTime, bot.Snoring, bot.Noise, bot.Hygiene, bot.Smoking,
+	((2*pow((top.Personality-bot.Personality),2))+
+	(2*pow((top.WakeupTime-bot.WakeupTime),2))+
+	(2*pow((top.SleepTime-bot.SleepTime),2))+
+	(2*pow((top.Snoring-bot.Snoring),2))+
+	(2*pow((top.Noise-bot.Noise),2))+
+	(2*pow((top.Hygiene-bot.Hygiene),2))+
+	(2*pow((top.Smoking-bot.Smoking),2))) as weighted
+	FROM profiles top, profiles bot WHERE top.KakaoID = ? and top.KakaoID != bot.KakaoID and bot.Gender = ? and bot.Dormitory = ? order by weighted");
+	$stmt->bind_param("iis", $KakaoID, $Gender, $Dormitory);
+	$stmt->execute();
+ 	$stmt->bind_result($Name, $Grade, $Introduce, $Profileimage, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking, $weighted);
+
+ 	$result = array();
+
+ 	while($stmt->fetch()){
+ 		$profile  = array();
+ 		$profile['Name'] = $Name;
+ 		$profile['Grade'] = $Grade;
+ 		$profile['Introduce'] = $Introduce;
+		$profile['Profileimage'] = $Profileimage;
+ 		$profile['Personality'] = $Personality;
+ 		$profile['WakeupTime'] = $WakeupTime;
+ 		$profile['SleepTime'] = $SleepTime;
+ 		$profile['Snoring'] = $Snoring;
+ 		$profile['Noise'] = $Noise;
+ 		$profile['Hygiene'] = $Hygiene;
+ 		$profile['Smoking'] = $Smoking;
+		$profile['weighted'] = $weighted;
+
+ 		array_push($result, $profile);
+     }
+
+     return $result;
+ }
+
+function filterp($KakaoID, $Gender, $Dormitory, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking) {
+		
+		$sql = "SELECT bot.Name, bot.Grade, bot.Introduce, bot.Profileimage, bot.Personality, bot.WakeupTime, bot.SleepTime, bot.Snoring, bot.Noise, bot.Hygiene, bot.Smoking,
+		((2*pow((top.Personality-bot.Personality),2))+ 
+		(2*pow((top.WakeupTime-bot.WakeupTime),2))+ 
+		(2*pow((top.SleepTime-bot.SleepTime),2))+
+		(2*pow((top.Snoring-bot.Snoring),2))+ 
+		(2*pow((top.Noise-bot.Noise),2))+
+		(2*pow((top.Hygiene-bot.Hygiene),2))+ 
+		(2*pow((top.Smoking-bot.Smoking),2))) as weighted
+		FROM profiles top, profiles bot WHERE top.KakaoID = ? and top.KakaoID != bot.KakaoID and bot.Gender = ? and bot.Dormitory = ? and bot.Personality regexp CONCAT('[',?,']') and 
+		bot.WakeupTime regexp CONCAT('[',?,']') and bot.SleepTime regexp CONCAT('[',?,']') and bot.Snoring regexp CONCAT('[',?,']') and bot.Noise regexp CONCAT('[',?,']') and 
+		bot.Hygiene regexp CONCAT('[',?,']') and bot.Smoking regexp CONCAT('[',?,']') order by weighted";
+		$stmt = $this->con->prepare($sql);
+		$stmt->bind_param("iisiiiiiii",$KakaoID, $Gender, $Dormitory, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking);
+		$stmt->execute();
+		$stmt->bind_result($Name, $Grade, $Introduce, $Profileimage, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking, $weighted);
+
+		$result = array(); 
+ 
+		while($stmt->fetch()){
+			$profile  = array();
+			$profile['Name'] = $Name; 
+			$profile['Grade'] = $Grade;
+			$profile['Introduce'] = $Introduce; 
+			$profile['Profileimage'] = $Profileimage;
+			$profile['Personality'] = $Personality;
+			$profile['WakeupTime'] = $WakeupTime;
+			$profile['SleepTime'] = $SleepTime; 
+			$profile['Snoring'] = $Snoring;
+			$profile['Noise'] = $Noise;
+			$profile['Hygiene'] = $Hygiene;
+			$profile['Smoking'] = $Smoking;
+		   $profile['weighted'] = $weighted;  
+   
+			array_push($result, $profile); 
+		}
+	
+		return $result; 
+
+	
+		}
+
+
+}
+
