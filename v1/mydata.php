21be60a70b1d400ce18a922f865cfa29da7f7d22
KookyZar <49061247+KookyZar@users.noreply.github.com>
2022-06-23 10:19:46 +0900

    Update mydata.php

--- v1/mydata.php
+++ v1/mydata.php
@@ -6,13 +6,11 @@ $username = "root";
 $password = "";
 $dbname = "roommating";
  
-// connect with database demo
 $conn = new mysqli($servername, $username, $password, $dbname);
  
- // an array to display response
  $response = array();
 
- // on below line we are checking if the parameter send is id or not.
+
  if(isset($_POST['KakaoID'],
 	$_POST['Personality'],
 	$_POST['WakeupTime'],
@@ -21,9 +19,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 	$_POST['Noise'],
 	$_POST['Hygiene'],
 	$_POST['Smoking'])){
-     // if the parameter send from the user id id then
-     // we will search the item for specific id.
-    
+
     
     
      
@@ -39,24 +35,20 @@ $conn = new mysqli($servername, $username, $password, $dbname);
      
 
     }
-        //on below line we are selecting the course detail with below id.
+    
     $stmt = $conn->prepare("UPDATE profiles SET Personality = ?, WakeupTime = ?, SleepTime = ?, Snoring = ?,Noise = ?, Hygiene = ?, Smoking = ? WHERE KakaoID = ?");
 	$stmt->bind_param("ssssssss", $inputPersonality, $inputWakeupTime, $inputSleepTime, $inputSnoring, $inputNoise, $inputHygiene, $inputSmoking, $inputKakaoID);
     $result = $stmt->execute();
-   // on below line we are checking if our
-   // table is having data with specific id.    
+  
    if($result == TRUE){
-         // if we get the response then we are displaying it below.
+      
          $response['error'] = false;
          $response['message'] = "Retrieval Successful!";
-         // on below line we are getting our result.
-         //$stmt->store_result();
-         // on below line we are passing parameters which we want to get.
+    
         
 
      } else{
-         // if the id entered by user donot exist then
-         // we are displaying the error message
+
          $response['error'] = true;
          $response['message'] = "Incorrect id";
      }
