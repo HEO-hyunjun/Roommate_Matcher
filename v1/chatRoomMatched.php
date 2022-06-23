d713618beb473670d4632b35859557011bef46f4
KookyZar <49061247+KookyZar@users.noreply.github.com>
2022-06-23 10:21:16 +0900

    Update chatRoomMatched.php

--- v1/chatRoomMatched.php
+++ v1/chatRoomMatched.php
@@ -6,16 +6,12 @@ $username = "root";
 $password = "";
 $dbname = "roommating";
 
-// connect with database demo
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
- // an array to display response
-	
-
- // on below line we are checking if the parameter send is id or not.
+ 
     if(isset($_POST['KakaoID1'], $_POST['KakaoID2'])){
-        // if the parameter send from the user id id then
-        // we will search the item for specific id.
+     
 
         $inputKakaoIDMe = $_POST['KakaoID1'];
         $inputKakaoIDOther = $_POST['KakaoID2'];
@@ -25,23 +21,21 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 	    $result = $stmt->execute();
 	    $stmt->bind_result($Matched);
          
-    //on below line we are selecting the course detail with below id.
+    
 
     if($result == TRUE){
-         // if we get the response then we are displaying it below.
+   
          $response['error'] = false;
          $response['message'] = "Successful!";
          $response['matched'] = $Matched;  
         }
    else{
-         // if the id entered by user donot exist then
-         // we are displaying the error message
+     
          $response['error'] = true;
          $response['message'] = "Incorrect id";
      }
     }
 
- // at last we are printing
- // all the data on below line.
+
  echo json_encode($response);
 ?>
