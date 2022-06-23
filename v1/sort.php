70fd4cb8f642dd40d0debad5bc68631f42e3156c
KookyZar <49061247+KookyZar@users.noreply.github.com>
2022-06-23 10:18:00 +0900

    Update sort.php

--- v1/sort.php
+++ v1/sort.php
@@ -6,13 +6,10 @@ $username = "root";
 $password = "";
 $dbname = "roommating";
  
-// connect with database demo
 $conn = new mysqli($servername, $username, $password, $dbname);
  
- // an array to display response
  $response = array();
 
- // on below line we are checking if the parameter send is id or not.
  if(isset($_POST['KakaoID'], $_POST['Gender'],$_POST['Dormitory'])){
     
   
@@ -33,19 +30,18 @@ $conn = new mysqli($servername, $username, $password, $dbname);
         FROM profiles top, profiles bot WHERE top.KakaoID = ? and top.KakaoID != bot.KakaoID and bot.Gender = ? and bot.Dormitory = ? order by weighted");
 	$stmt->bind_param("sss", $inputKakaoID, $inputGender, $inputDormitory);
         $result = $stmt->execute();
-   // on below line we are checking if our
-   // table is having data with specific id.    
+ 
    if($result == TRUE){
-         // if we get the response then we are displaying it below.
+  
          $response['error'] = false;
          $response['message'] = "Retrieval Successful!";
-         // on below line we are getting our result.
+
          $stmt->store_result();
-         // on below line we are passing parameters which we want to get.
+
          $stmt->bind_result($Name, $Grade, $Introduce, $Profileimage, $Personality, $WakeupTime, $SleepTime, $Snoring, $Noise, $Hygiene, $Smoking, $weighted);
-         // on below line we are fetching the data.
+
          
-         //Fetch into associative array
+     
          while ($stmt->fetch()) {          
             $profile  = array();
 			$profile['Name'] = $Name; 
@@ -65,21 +61,16 @@ $conn = new mysqli($servername, $username, $password, $dbname);
        } 
 
      } else{
-         // if the id entered by user donot exist then
-         // we are displaying the error message
+ 
          $response['error'] = true;
          $response['message'] = "Incorrect id";
      }
     
   /*else{
-      // if the user donot adds any parameter while making request
-      // then we are displaying the error as insufficient parameters.
-      $response['error'] = true; */
-     
- 
+      
+      $response['error'] = true; */ 
 
  
- // at last we are printing
- // all the data on below line.
+
  echo json_encode($response);
 ?>
