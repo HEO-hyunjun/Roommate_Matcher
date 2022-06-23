8f8c9e8db9b1d4c07e5e5f1903a3567471768b0f
KookyZar <49061247+KookyZar@users.noreply.github.com>
2022-06-23 10:21:43 +0900

    Update chatRoomDel.php

--- v1/chatRoomDel.php
+++ v1/chatRoomDel.php
@@ -5,17 +5,15 @@ $username = "root";
 $password = "";
 $dbname = "roommating";
 
-// connect with database demo
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
- // an array to display response
 
  $isAlready = 0;
 
- // on below line we are checking if the parameter send is id or not.
+ 
     if(isset($_POST['KakaoID1'], $_POST['KakaoID2'])){
-        // if the parameter send from the user id id then
-        // we will search the item for specific id.
+  
 
         $inputKakaoIDMe = $_POST['KakaoID1'];
         $inputKakaoIDOther = $_POST['KakaoID2'];
@@ -27,19 +25,16 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 
    if($result == TRUE){
-         // if we get the response then we are displaying it below.
+       
          $response['error'] = false;
          $response['message'] = "delete Successful!";
             }
    else{
-         // if the id entered by user donot exist then
-         // we are displaying the error message
+        
          $response['error'] = true;
          $response['message'] = "Incorrect id";
      }
 
- // at last we are printing
- // all the data on below line.
  echo json_encode($response);
 ?>
                                                                                                                                 66,2          Bot
