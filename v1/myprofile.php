ff91ef16d96c071258bd895bbe4852fbeaff3089
KookyZar <49061247+KookyZar@users.noreply.github.com>
2022-06-23 10:19:18 +0900

    Update myprofile.php

--- v1/myprofile.php
+++ v1/myprofile.php
@@ -6,13 +6,11 @@ $username = "root";
 $password = "";
 $dbname = "roommating";
  
-// connect with database demo
+
 $conn = new mysqli($servername, $username, $password, $dbname);
  
- // an array to display response
  $response = array();
 
- // on below line we are checking if the parameter send is id or not.
  if(isset($_POST['KakaoID'],
     $_POST['Name'],
 	$_POST['Grade'],
@@ -20,9 +18,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 	$_POST['Dormitory'],
 	$_POST['Introduce'],
 	$_POST['Profileimage'])){
-     // if the parameter send from the user id id then
-     // we will search the item for specific id.
-    
     
     
      
@@ -37,37 +32,29 @@ $conn = new mysqli($servername, $username, $password, $dbname);
      
 
     }
-        //on below line we are selecting the course detail with below id.
+ 
     $stmt = $conn->prepare("UPDATE profiles SET  Name = ?, Grade = ?, Gender = ?, Dormitory = ?, Introduce = ?, Profileimage = ? WHERE KakaoID = ?");
 	$stmt->bind_param("sssssss", $inputName, $inputGrade, $inputGender, $inputDormitory, $inputIntroduce, $inputProfileimage, $inputKakaoID);
     $result = $stmt->execute();
-   // on below line we are checking if our
-   // table is having data with specific id.    
+  
    if($result == TRUE){
-         // if we get the response then we are displaying it below.
+     
          $response['error'] = false;
          $response['message'] = "Retrieval Successful!";
-         // on below line we are getting our result.
-         //$stmt->store_result();
-         // on below line we are passing parameters which we want to get.
+     
         
 
      } else{
-         // if the id entered by user donot exist then
-         // we are displaying the error message
+  
          $response['error'] = true;
          $response['message'] = "Incorrect id";
      }
     
   /*else{
-      // if the user donot adds any parameter while making request
-      // then we are displaying the error as insufficient parameters.
+
       $response['error'] = true; */
      
  
 
- 
- // at last we are printing
- // all the data on below line.
  echo json_encode($response);
 ?>
